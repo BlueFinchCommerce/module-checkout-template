071ff4982ce334924ae794703f36616e802a3c2a
Alla Nehaenko <allanehaenko@gmail.com>
2025-01-15 10:54:00 +0000

    renamed everything that related to gene better checkout

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Gene_BetterCheckoutNewModule',
+    'BlueFinch_CheckoutNewModule',
     __DIR__
 );
