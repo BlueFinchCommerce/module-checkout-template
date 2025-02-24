1f491e731339af0ced26762fd669de1baabeebd4
Max Parker <max.parker@gene.co.uk>
2025-02-24 15:30:34 +0000

    Added example express payment method

---
+++ view/frontend/web/js/checkout/src/components/expressPayment/expressPayment.vue
@@ -0,0 +1,32 @@
+<template>
+  <p class="new-component">New Component - Express Payment</p>
+</template>
+
+<script>
+import loadFromCheckout from '../../helpers/loadFromCheckout';
+
+export default {
+  name: 'Express Payment',
+  data() {
+    return {
+      key: 'exampleExpressMethod',
+    };
+  },
+  async created() {
+    const [
+      cartStore,
+      paymentStore,
+    ] = await loadFromCheckout([
+      'stores.useCartStore',
+      'stores.useConfigStore',
+      'stores.usePaymentStore',
+    ]);
+
+    paymentStore.addExpressMethod(this.key);
+
+    await cartStore.getCart();
+
+    console.log(cartStore.cart);
+  }
+};
+</script>
